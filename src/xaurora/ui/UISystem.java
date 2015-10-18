c41aed9fcb7377c04654f1080fe4600681879850
KHLee <khlee1886@gmail.com>
2015-10-18 14:25:13 +0800

    initialize ui package

---
+++ src/xaurora/ui/UISystem.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xaurora.ui;
+
+/**
+ *
+ * @author Lee
+ */
+public class UISystem {
+    
+}
