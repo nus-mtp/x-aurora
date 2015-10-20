8a4e7e9f98c04004d797601f7c2bfcdee14c6fd0
Michael <khlee1886@gmail.com>
2015-10-20 14:59:11 +0800

    added dropbox icon to login UI

--- src/xaurora/ui/LoginUI.java
+++ src/xaurora/ui/LoginUI.java
@@ -35,45 +35,46 @@ public class LoginUI extends Application{
 
         GridPane grid = new GridPane();
         grid.setHgap(50);
-        grid.setVgap(10);
+        grid.setVgap(20);
         grid.setPadding(new Insets(10, 10, 10, 10));
         grid.setAlignment(Pos.CENTER);
         
+        Image image = new Image("File:dropbox.png");
+        ImageView imageView = new ImageView(image);
+        imageView.setFitHeight(140);
+        imageView.setFitWidth(140);
+        grid.add(imageView, 0, 0, 2, 4);
+        
         TextField usernameTextField = new TextField();
         usernameTextField.setPromptText("Email");
-        grid.add(usernameTextField, 0, 0, 2, 1);
+        grid.add(usernameTextField, 2, 0, 2, 1);
         PasswordField passwordField = new PasswordField();
         passwordField.setPromptText("Password");
-        grid.add(passwordField, 0, 1, 2, 1);
+        grid.add(passwordField, 2, 1, 2, 1);
        
         CheckBox remember = new CheckBox("remember me");
-        grid.add(remember, 0, 2);
+        grid.add(remember, 2, 2);
         Button login = new Button("Login");
-        grid.add(login, 1, 2);
+        grid.add(login, 3, 2);
        
         Hyperlink forgotPassword = new Hyperlink();
         forgotPassword.setText("forgot password?");
-        grid.add(forgotPassword, 0, 3);
+        grid.add(forgotPassword, 2, 3);
         Hyperlink register = new Hyperlink();
         register.setText("register");
-        grid.add(register, 1, 3);
+        grid.add(register, 3, 3);
         
         HBox hbox = new HBox();
-        hbox.setPadding(new Insets(15, 12, 15, 12));
+        hbox.setPadding(new Insets(15, 10, 15, 10));
         hbox.setSpacing(20);
+        hbox.setAlignment(Pos.CENTER_RIGHT);
         Label warning = new Label("Cross device copy paste will not be available without loggin in");
+        warning.setWrapText(true);
         Button skip = new Button("skip login");
         hbox.getChildren().addAll(warning, skip);
-        hbox.setAlignment(Pos.CENTER_RIGHT);
-        
-        VBox vbox = new VBox();
-        //Image image = new Image("dropbox.png");
-        //ImageView imageView = new ImageView(image);
-        //vbox.getChildren().add(imageView);
-      
+
         borderPane.setCenter(grid);
         borderPane.setBottom(hbox);
-        borderPane.setLeft(vbox);
         
         Scene scene = new Scene(borderPane, 500, 300);
         primaryStage.setScene(scene);
