8135ce712dac375010ab1ac25b5bd725fecefa12
Michael <khlee1886@gmail.com>
2015-10-27 14:01:55 +0800

    skeleton for preference UI

---
+++ src/xaurora/ui/PreferenceUI.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xaurora.ui;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.geometry.Pos;
+import javafx.geometry.Side;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Lee
+ */
+public class PreferenceUI extends Application{
+    public static void main(String[] args) {
+        launch(args);
+    }
+     
+    @Override
+    public void start(Stage stage){
+        stage.setTitle("x-aurora");      
+        Scene scene = createScene();
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+    private Scene createScene(){
+        BorderPane border = new BorderPane();
+        
+        TabPane tabs = new TabPane();
+        Tab tabSetting = new Tab("Setting");
+        tabSetting.setContent(createSettingPane());
+        Tab tabTutorial = new Tab("Tutorial");
+        Tab tabAboutUs = new Tab("About Us");
+        Tab tabDataManaging = new Tab("Data Managing");
+        tabs.getTabs().addAll(tabSetting, tabTutorial, tabAboutUs, tabDataManaging);
+        tabs.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
+        
+        Image image = new Image("File:dropbox.png");
+        ImageView imageView = new ImageView(image);
+        imageView.setFitHeight(50);
+        imageView.setFitWidth(50);
+        Label email = new Label("user@example.com");
+        email.setGraphic(imageView);
+        
+        HBox hbox = new HBox();
+        hbox.getChildren().addAll(tabs, email);
+        
+        border.setTop(tabs);
+        
+        Scene scene = new Scene(border, 500, 300);
+        return scene;
+    }
+    
+    private TabPane createSettingPane(){
+        TabPane tabs = new TabPane();
+        Tab tabSystem = new Tab("System");
+        tabSystem.setContent(createSystemPane());
+        Tab tabHotkeys = new Tab("Hotkeys");
+        Tab tabBlockedList = new Tab("Blocked List");
+        Tab tabDropbox = new Tab("Dropbox");
+        Tab tabPath = new Tab("Path");
+        Tab tabStorage = new Tab("Storage");
+        tabs.getTabs().addAll(tabSystem, tabHotkeys, tabBlockedList, tabDropbox, tabPath, tabStorage);
+        tabs.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
+        //tabs.setSide(Side.LEFT);
+
+        return tabs;
+    } 
+    
+    private GridPane createSystemPane(){
+        GridPane grid = new GridPane();
+        Label label1 = new Label("Run on start up");
+        Label label2 = new Label("Hide in toolbar when close");
+        
+        return grid;
+    }
+}
