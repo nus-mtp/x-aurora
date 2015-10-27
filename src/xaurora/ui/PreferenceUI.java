54f662bc6f9e259e17109a92849fba566b4db9f3
Michael <khlee1886@gmail.com>
2015-10-27 15:22:23 +0800

    created text editor pane UI

--- src/xaurora/ui/PreferenceUI.java
+++ src/xaurora/ui/PreferenceUI.java
@@ -12,7 +12,11 @@ import javafx.geometry.Pos;
 import javafx.geometry.Side;
 import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.image.Image;
@@ -20,6 +24,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /**
@@ -72,11 +77,13 @@ public class PreferenceUI extends Application{
         Tab tabSystem = new Tab("System");
         tabSystem.setContent(createSystemPane());
         Tab tabHotkeys = new Tab("Hotkeys");
+        Tab tabTextEditor = new Tab("Text Editor");
+        tabTextEditor.setContent(createTextEditorPane());
         Tab tabBlockedList = new Tab("Blocked List");
         Tab tabDropbox = new Tab("Dropbox");
         Tab tabPath = new Tab("Path");
         Tab tabStorage = new Tab("Storage");
-        tabs.getTabs().addAll(tabSystem, tabHotkeys, tabBlockedList, tabDropbox, tabPath, tabStorage);
+        tabs.getTabs().addAll(tabSystem, tabHotkeys, tabTextEditor, tabBlockedList, tabDropbox, tabPath, tabStorage);
         tabs.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
         //tabs.setSide(Side.LEFT);
 
@@ -86,7 +93,7 @@ public class PreferenceUI extends Application{
     private GridPane createSystemPane(){
         GridPane grid = new GridPane();
         grid.setHgap(50);
-        grid.setVgap(10);
+        grid.setVgap(15);
         grid.setPadding(new Insets(10, 10, 10, 10));
         grid.setAlignment(Pos.CENTER);
         
@@ -98,6 +105,51 @@ public class PreferenceUI extends Application{
         grid.add(checkbox1, 1, 0);
         grid.add(label2, 0, 1);
         grid.add(checkbox2, 1, 1);
+        
+        return grid;
+    }
+    
+    private GridPane createTextEditorPane(){
+        GridPane grid = new GridPane();
+        grid.setHgap(50);
+        grid.setVgap(15);
+        grid.setPadding(new Insets(10, 10, 10, 10));
+        grid.setAlignment(Pos.CENTER);
+        
+        Label label1 = new Label("Number of matching text displayed");
+        Label label2 = new Label("Show source of text");
+        Label label3 = new Label("Box Colour");
+        Label label4 = new Label("Box Transparency");
+        
+        Spinner spinner = new Spinner();
+        SpinnerValueFactory svf = new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 20);
+        spinner.setValueFactory(svf);
+        spinner.increment(4);
+        spinner.setEditable(true);
+        spinner.setPrefWidth(70);
+        
+        CheckBox checkbox = new CheckBox();
+        
+        ColorPicker colorPicker = new ColorPicker();
+        colorPicker.setValue(Color.WHITE);
+        colorPicker.setStyle("-fx-color-label-visible: false;");
+        
+        Slider transparency = new Slider();
+        transparency.setMin(0);
+        transparency.setMax(100);
+        transparency.setValue(0);
+        transparency.setShowTickLabels(true);
+        transparency.setShowTickMarks(true);
+        transparency.setMajorTickUnit(50);
+      
+        grid.add(label1, 0, 0);
+        grid.add(label2, 0, 1);
+        grid.add(label3, 0, 2);
+        grid.add(label4, 0, 3);
+        grid.add(spinner, 1, 0);
+        grid.add(checkbox, 1, 1);
+        grid.add(colorPicker, 1, 2);
+        grid.add(transparency, 1, 3);
         
         return grid;
     }
