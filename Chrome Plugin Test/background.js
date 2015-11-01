ec9f0d9f655e35fadd186c3fbcacf0c4d1671c13
Raven <elwinjohann@gmail.com>
2015-11-01 14:29:53 +0800

    Chrome Plugin Test V0.2
    Some test on extracting and sending text contents

--- Chrome Plugin Test/background.js
+++ Chrome Plugin Test/background.js
@@ -1,51 +1,56 @@
-
-
 /*
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
     chrome.tabs.sendMessage(tabs[0].id, {action: "SendIt"}, function(response) {});  
 });
 */
+
+
 var serverStat = "unconnected";
+var serverPort = '6789';
+var dataPort = '6789';
 var connector = {
     onLoad: function(){
-        setInterval(function(){ connector.getConnection() },3000);
+        setInterval(function(){ connector.getConnection(); },3000);
     },
     
-    getConnection: function(){
+    getConnection: function(){        
         var request = new XMLHttpRequest();
         request.onreadystatechange = function() {
             if (request.readyState == 4) {
-                alert(request.responseText);
+                var respond = request.responseText;
             }
         }
         request.addEventListener("load", connector.connectionSuccess, false);  
 		request.addEventListener("error", connector.connectionFail, false);
-        request.open('GET', 'http://127.0.0.1:6789', true); 
-		request.send('Hello Server!');
+        request.open('PUSH', 'http://127.0.0.1:'+serverPort, true); 
+		request.send("Hello Server!\nI want to shake hands!\n");
+        request.timeout = 3000;
     },
     
     connectionSuccess: function(){
         serverStat = "connected";
-        textExtractor.pushText();
     },
     
     connectionFail: function(){
-        alert("Connection Lost!");  
         serverStat = "unconnected";
     }
 };
 
 var textExtractor = {
-    pushText: function(){
-        var request = new XMLHttpRequest();
-        request.onreadystatechange = function() {
-            if (request.readyState == 4) {
-                alert(request.responseText);
-            }
+    pushText: function(tabid){
+        if (serverStat == "connected") {
+            chrome.tabs.sendMessage(tabid, {action: "getDOM"}, function(response) {
+                var request = new XMLHttpRequest();
+                console.log("Sending Content");
+                request.open('PUSH', 'http://127.0.0.1:'+dataPort, true); 
+                request.send(response.extracted);
+            });
         }
-        request.open('PUT', 'http://127.0.0.1:6789', true); 
-		request.send('This is a plain text.');
     }
 }
+
+chrome.tabs.onActivated.addListener(function (info){
+    textExtractor.pushText(info.tabId);
+});
 
 connector.onLoad();
