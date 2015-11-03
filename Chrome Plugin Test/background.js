2b176fbf8d6da7d12a7fb6e40b239aa906ba73bb
Raven <elwinjohann@gmail.com>
2015-11-03 11:25:38 +0800

    Revert "Chrome Plugin Test V0.2"
    This reverts commit ec9f0d9f655e35fadd186c3fbcacf0c4d1671c13.

--- Chrome Plugin Test/background.js
+++ Chrome Plugin Test/background.js
@@ -1,56 +1,51 @@
+
+
 /*
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
     chrome.tabs.sendMessage(tabs[0].id, {action: "SendIt"}, function(response) {});  
 });
 */
-
-
 var serverStat = "unconnected";
-var serverPort = '6789';
-var dataPort = '6789';
 var connector = {
     onLoad: function(){
-        setInterval(function(){ connector.getConnection(); },3000);
+        setInterval(function(){ connector.getConnection() },3000);
     },
     
-    getConnection: function(){        
+    getConnection: function(){
         var request = new XMLHttpRequest();
         request.onreadystatechange = function() {
             if (request.readyState == 4) {
-                var respond = request.responseText;
+                alert(request.responseText);
             }
         }
         request.addEventListener("load", connector.connectionSuccess, false);  
 		request.addEventListener("error", connector.connectionFail, false);
-        request.open('PUSH', 'http://127.0.0.1:'+serverPort, true); 
-		request.send("Hello Server!\nI want to shake hands!\n");
-        request.timeout = 3000;
+        request.open('GET', 'http://127.0.0.1:6789', true); 
+		request.send('Hello Server!');
     },
     
     connectionSuccess: function(){
         serverStat = "connected";
+        textExtractor.pushText();
     },
     
     connectionFail: function(){
+        alert("Connection Lost!");  
         serverStat = "unconnected";
     }
 };
 
 var textExtractor = {
-    pushText: function(tabid){
-        if (serverStat == "connected") {
-            chrome.tabs.sendMessage(tabid, {action: "getDOM"}, function(response) {
-                var request = new XMLHttpRequest();
-                console.log("Sending Content");
-                request.open('PUSH', 'http://127.0.0.1:'+dataPort, true); 
-                request.send(response.extracted);
-            });
+    pushText: function(){
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = function() {
+            if (request.readyState == 4) {
+                alert(request.responseText);
+            }
         }
+        request.open('PUT', 'http://127.0.0.1:6789', true); 
+		request.send('This is a plain text.');
     }
 }
-
-chrome.tabs.onActivated.addListener(function (info){
-    textExtractor.pushText(info.tabId);
-});
 
 connector.onLoad();
--- Chrome Plugin Test/content.js
+++ Chrome Plugin Test/content.js
@@ -1,33 +1,10 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    sendResponse({extracted: document.body.innerText});
+/*
+chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
+   if (msg.action == 'SendIt') {
+      alert("Message recieved!");
+      alert(sender);
+   }
 });
+*/
 
-// Get the text within an element
-// Doesn't do any normalising, returns a string
-// of text as found.
-function getText(element) {
-  var text = [];
-  var self = arguments.callee;
-  var el, els = element.childNodes;
-  var excluded = {
-    'noscript': 'noscript',
-    'script'  : 'script'
-  };
-
-  for (var i=0, iLen=els.length; i<iLen; i++) {
-    el = els[i];
-
-    // May need to add other node types here
-    if ( el.nodeType == 1 && 
-       !(el.tagName.toLowerCase() in excluded)) {
-      text.push(self(el));
-
-    // If working with XML, add nodeType 4 to get text from CDATA nodes
-    } else if (el.nodeType == 3) {
-
-      // Deal with extra whitespace and returns in text here.
-      text.push(el.data);
-    }
-  }
-  return text.join('');
-}
+//chrome.extention.addEventListener("load", connector.onLoad, false);
