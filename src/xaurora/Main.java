cb80c9c9ce5bb03da2d5b078c14e81bc4c647975
MartinGaoR <a0101891@u.nus.edu>
2015-11-08 13:40:59 +0800

    refine Server

--- src/xaurora/Main.java
+++ src/xaurora/Main.java
@@ -1,9 +1,10 @@
 package xaurora;
 import xaurora.communication.*;
+
 import java.util.*;
 public class Main {
 	public static void main(String[] args){
-		Server server = new Server(6789);
+		SimpleServer server = new SimpleServer(6789);
 		Thread serverThread = new Thread(server);
 		serverThread.start();
 	}
