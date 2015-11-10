f0c3f45b4dab53b298a0bdc0032ec31f731a405b
Michael <khlee1886@gmail.com>
2015-11-10 19:43:41 +0800

    push to pull

--- src/xaurora/Main.java
+++ src/xaurora/Main.java
@@ -1,33 +1,36 @@
 package xaurora;
+
 import xaurora.communication.*;
 import xaurora.security.Security;
 import xaurora.system.SystemManager;
 import xaurora.ui.LoginUI;
 
-
 public class Main {
-	public static void main(String[] args){
-		//ChromeServer chromeSvr = new ChromeServer(6789);
-		//WordServer wordSvr = new WordServer(23333);
-		
-		//Thread chromeSvrThread = new Thread(chromeSvr);
-		//Thread wordSvrThread = new Thread(wordSvr);
-		
-		//chromeSvrThread.start();
-		//wordSvrThread.start();
-		//SystemManager s = SystemManager.getInstance();
-                
-        //LoginUI.main(null);
-		Security s = Security.getInstance();
-		try{
-			s.encrypt("abcdefg".getBytes("UTF-8"));
-			for(int i =0;i<s.decrypt(s.encrypt("abcdefg".getBytes("UTF-8"))).length;i++){
-				System.out.print((char)s.decrypt(s.encrypt("abcdefg".getBytes("UTF-8")))[i]);
-			}
-		} catch(Exception e){
-			e.printStackTrace();
-		}
-	}
 
+    public static void main(String[] args) {
+        /***
+        ChromeServer chromeSvr = new ChromeServer(6789);
+        WordServer wordSvr = new WordServer(23333);
+
+        Thread chromeSvrThread = new Thread(chromeSvr);
+        Thread wordSvrThread = new Thread(wordSvr);
+
+        chromeSvrThread.start();
+        wordSvrThread.start();
+        SystemManager s = SystemManager.getInstance();
+        LoginUI.main(null);
+        * ***/
+
+        Security s = Security.getInstance();
+        try {
+            s.encrypt("abcdefg".getBytes("UTF-8"));
+            for (int i = 0; i < s.decrypt(s.encrypt("abcdefg".getBytes("UTF-8"))).length; i++) {
+                System.out.print((char) s.decrypt(s.encrypt("abcdefg".getBytes("UTF-8")))[i]);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 
 }
